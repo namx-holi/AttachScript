af543a467f5fde6080849e85ec41496badbbabda
namx-holi <chucktesta@live.com.au>
2018-02-16 17:23:27 +1100

    Moved example scripts into new folder
    Example scripts are now in a folder called example scripts

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 replacing = "C:/Program Files (x86)/Microsoft Office/root/Office16/WINWORD.EXE"
 
-on_startup = "example_script.py"
-on_closing = "example_script.vbs"
+on_startup = "example scripts/example_script.py"
+on_closing = "example scripts/example_script.vbs"
